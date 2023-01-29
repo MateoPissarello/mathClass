b02284a17008f260e3fb0eccc5ad6051facb4c6c
MateoPissarello <mateopissa@gmail.com>
2023-01-28 21:07:14 -0500

    Math Operations in Java

---
+++ src/Main.java
@@ -0,0 +1,25 @@
+
+public class Main {
+    public static void main(String[] args) {
+        double x = 3.14;
+        double z = 16
+        double y = -10;
+
+        double maximus = Math.max(x, y);
+        double minimum = Math.min(x, y);
+        double absolute = Math.abs(y);
+        double square = Math.sqrt(z);
+        double round = Math.round(x); //round down
+        double ceil = Math.ceil(x); //round up
+
+        System.out.println(maximus);
+        System.out.println(minimum);
+        System.out.println(absolute);
+        System.out.println(square);
+        System.out.println(round);
+        System.out.println(ceil);
+    }
+
+    }
+
+}
